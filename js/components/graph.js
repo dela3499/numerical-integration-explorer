6caf9c2c054fb6a350462a0a39255de9c796894c
Carlos <dela3499@gmail.com>
2014-07-28 17:06:22 -0500

    Successfully refactored graph component to use EaselJS internally.

--- js/components/graph.js
+++ js/components/graph.js
@@ -2,6 +2,9 @@
 
 var Graph = React.createClass({
     componentDidMount: function () {   
+        var canvas = this.getDOMNode(); 
+        var stage = new createjs.Stage(canvas);
+        this.setState({stage:stage});
         this.draw();
     },
     componentDidUpdate: function () {
@@ -22,31 +25,27 @@ var Graph = React.createClass({
                 this.props.size,
                 this.props.size,
                 this.props.bounds
-                ),
-            c = this.getDOMNode().getContext('2d');
-        var t = this;
-        c.clearRect(0,0,this.props.size,this.props.size);
+                );
         // Plot each dataset
         for (var i = 0; i < plotData.length; i++) {
             var dataSet = plotData[i];
-            c.strokeStyle = dataSet.options.color || "white";
-            c.lineWidth = dataSet.options.lineWidth || 3;
-            c.beginPath();
+            var line = new createjs.Shape();
+            line.graphics.setStrokeStyle(dataSet.options.lineWidth || 3);
+            line.graphics.beginStroke(dataSet.options.color || "white");
+            
             // Plot each point in dataset (connected with lines)
             for (var j = 0; j < dataSet.x.length - 1; j++) {
-                c.moveTo(dataSet.x[j], dataSet.y[j]);
-                c.lineTo(dataSet.x[j+1], dataSet.y[j+1]);
+                line.graphics.moveTo(dataSet.x[j], dataSet.y[j]);
+                line.graphics.lineTo(dataSet.x[j+1], dataSet.y[j+1]);
             }
-            c.closePath();
-            c.stroke();
+            line.graphics.endStroke();
+            var stage = this.state.stage;
+            stage.removeAllChildren();
+            stage.addChild(line);
+            stage.update();
         };
     }           
 });
-
-
-
-
-
 
 
 
